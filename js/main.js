d69020b604c3e2511e41676dc365bc8e47d97000
Phil Sanders <philsanders@Phil-2.local>
2016-07-18 10:32:46 -0400

    First commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'toader', {
+	preload: preload,
+	create: create,
+	update: update,
+	render: render
+});
+
+function preload() {
+
+}
+
+function create(){
+
+}
+
+function update() {
+
+}
+
+function render() {
+
+}
