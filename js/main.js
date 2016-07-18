fb0c1a74134c4bdc7ba8d61cbab495ce83aea289
Phil Sanders <philsanders@Phil-2.local>
2016-07-18 10:33:13 -0400

    Preloading assets

--- js/main.js
+++ js/main.js
@@ -7,7 +7,11 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'toader', {
 });
 
 function preload() {
-
+	game.load.image('player', 'assets/img/map1.01.png');
+    game.load.image('player', 'assets/img/toad.png');
+    game.load.image('car', 'assets/img/car1.png');
+	game.load.image('bullet', 'assets/img/bullet.png');
+	game.load.spritesheet('explosion', 'assets/img/explosion1.png', 142, 200, 16);
 }
 
 function create(){
