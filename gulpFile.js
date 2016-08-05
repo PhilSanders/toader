5612cbdc361b5bc0bce443bdecbd57c19f44054f
Phil Sanders <philsanders@Phil-2.local>
2016-08-05 12:16:03 -0400

    Added gulp-useref

--- gulpFile.js
+++ gulpFile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-	webserver = require('gulp-webserver');
+	webserver = require('gulp-webserver'),
+	useref = require('gulp-useref');
 
 gulp.task('webserver', function() {
   gulp.src('.')
@@ -9,4 +10,10 @@ gulp.task('webserver', function() {
       open: true,
 	  fallback: 'index.html'
     }));
+});
+
+gulp.task('build', function(){
+	return gulp.src('./*.html')
+        .pipe(useref())
+        .pipe(gulp.dest('dist'));
 });
