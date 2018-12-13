e3a4cd49bfa0dbc1d15d63727782f1c920449a75
PhilSanders79 <phil@sourcetoad.com>
2018-12-13 12:01:15 -0500

    more clean up

--- gulpFile.js
+++ gulpFile.js
@@ -3,19 +3,13 @@ var gulp = require('gulp'),
     webserver = require('gulp-webserver'),
     useref = require('gulp-useref');
 
-gulp.task('webserver', function() {
-  gulp.src('.')
-    .pipe(webserver({
-      livereload: true,
-      directoryListing: false,
-      open: true,
-    fallback: 'index.html'
-    }));
-});
-
 gulp.task('move-assets', function() {
   return gulp.src(['assets/**'])
       .pipe(gulp.dest('dist/assets'))
+});
+
+gulp.task('clean', function() {
+  return del(['./dist']);
 });
 
 gulp.task('build', ['clean', 'move-assets'], function(){
@@ -24,8 +18,14 @@ gulp.task('build', ['clean', 'move-assets'], function(){
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean', function() {
-  return del(['./dist']);
+gulp.task('webserver', function() {
+  gulp.src('.')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: false,
+      open: true,
+    fallback: 'index.html'
+    }));
 });
 
 gulp.task('default', function() {
